95c0b75f4b13807efe4a443244b712496bc9ec49
bunderontheway <bunderontheway@gmail.com>
2021-07-14 09:02:55 +0700

    tambah file

---
+++ Section 7 - ProductCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ProcuctCell: UICollectionViewCell {
+	@IBOutlet weak var productImage: UIImageView!
+	@IBOutlet weak var productTitle: UILabel!
+	@IBOutlet weak var productPrice: UILabel!
+
+}
